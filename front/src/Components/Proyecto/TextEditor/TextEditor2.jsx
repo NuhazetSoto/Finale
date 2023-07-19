1cf38670ba4ff93bbddaf77ae560868b6ce50a4a
Nuhazet <nuhazet1997@gmail.com>
2023-07-19 11:00:53 +0100

    editor done

---
+++ front/src/Components/Proyecto/TextEditor/TextEditor2.jsx
@@ -0,0 +1,78 @@
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import "./TextEditor.css";
+import {
+  findProyecto,
+  updateContent,
+} from "../../../Services/proyecto.services";
+
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ font: [] }],
+    [{ size: [] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+  ],
+};
+
+function TextEditor2({ proyecto }) {
+  const [content, setContent] = useState(proyecto.contenido);
+  const { id } = useParams();
+  const navigation = useNavigate();
+  const usuarioId = localStorage.getItem("id");
+
+  const handleContentChange = (value) => {
+    setContent(value);
+  };
+
+  const handleContentUpdate = async () => {
+   
+    const proyectoData = await updateContent(
+      {
+        titulo: proyecto.titulo,
+        sinopsis: proyecto.sinopsis,
+        contenido: content,
+        portada: proyecto.portada,
+        usuario_id: proyecto.usuario_id,
+        genero_id: proyecto.genero_id,
+        created_at: proyecto.created_at,
+        updated_at: proyecto.updated_at,
+      },
+      proyecto.id
+    );
+    navigation(`/usuario/${usuarioId}/misproyectos`);
+    console.log(proyectoData);
+  };
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="editor">
+          <ReactQuill
+            theme="snow"
+            value={content}
+            onChange={handleContentChange}
+            className="editor-input"
+          />
+        </div>
+        <div className="preview">
+          <div dangerouslySetInnerHTML={{ __html: content }} />
+
+          <button className="add-page-btn" onClick={handleContentUpdate}>
+            Add Page
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TextEditor2;
